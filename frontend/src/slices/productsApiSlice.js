16bc935fd4b836f6ce08c3ec884fa7cf75c1078a
Lerato - 220076073 <35888479+Lerato007@users.noreply.github.com>
2024-01-11 18:09:23 +0200

    Edit Product

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -13,12 +13,21 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: (productId) => ({
         url: `${PRODUCTS_URL}/${productId}`,
       }),
+      providesTags: ["Product"],
       keepUnusedDataFor: 5,
     }),
     createProduct: builder.mutation({
       query: () => ({
         url: PRODUCTS_URL,
         method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: "PUT",
+        body: data,
       }),
       invalidatesTags: ["Product"],
     }),
@@ -29,4 +38,5 @@ export const {
   useGetProductsQuery,
   useGetProductDetailsQuery,
   useCreateProductMutation,
+  useUpdateProductMutation,
 } = productsApiSlice;
