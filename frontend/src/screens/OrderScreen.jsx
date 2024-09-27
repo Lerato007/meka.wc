f4083b6642b34473d5a9bb14820e5f285c368e68
Lerato007 <leratomoshabi500@gmail.com>
2024-09-27 14:04:47 +0200

    Approved payment page

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -41,7 +41,7 @@ const OrderScreen = () => {
   const generatePayFastForm = () => {
     const merchantId = "10000100";
     const merchantKey = "46f0cd694581a";
-    const returnUrl = "https://meka-wc.onrender.com/";
+    const returnUrl = "https://meka-wc.onrender.com/success";
     const cancelUrl = "https://meka-wc.onrender.com/";
     const notifyUrl = "https://meka-wc.onrender.com/";
 
