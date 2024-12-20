afa4062acd87c3ca90db3c70091684e3a5cc54d9
Lerato007 <leratomoshabi500@gmail.com>
2024-12-20 13:28:04 +0200

    First launch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-/*** CODE STARTS HERE ***/
+/** CODE STARTS HERE **/
 const App = () => {
   return (
     <>
